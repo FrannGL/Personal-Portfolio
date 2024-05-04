38a9b39f1a1e22507035a04b8fd7736033265829
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-04 20:51:46 -0300

    FIX reveal effect

--- src/routes/Portfolio/Card/index.tsx
+++ src/routes/Portfolio/Card/index.tsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.scss";
 import GlassIcon from "@/components/Icons/Glass";
 import RedirectIcon from "@/components/Icons/Redirect";
 import Link from "next/link";
+import { Fade } from "react-awesome-reveal";
 
 interface CardProps {
 	project: Project;
@@ -11,25 +12,27 @@ interface CardProps {
 
 const Card = ({ project }: CardProps) => {
 	return (
-		<div className={styles.card}>
-			<Image src={project.image} className={styles.image} alt='Preview' />
-			<div className={styles.overlay}>
-				<div className={styles.titles}>
-					<h3 className={styles.title}>
-						{project.title} | <span>{project.category}</span>
-					</h3>
-					<p className={styles.description}>{project.description}</p>
-				</div>
-				<div className={styles.btn_container}>
-					<Link href={project.repository} className={styles.link} target='_blank'>
-						<GlassIcon />
-					</Link>
-					<Link href={project.url} className={styles.link} target='_blank'>
-						<RedirectIcon stroke='#16c0f0' />
-					</Link>
+		<Fade>
+			<div className={styles.card}>
+				<Image src={project.image} className={styles.image} alt='Preview' />
+				<div className={styles.overlay}>
+					<div className={styles.titles}>
+						<h3 className={styles.title}>
+							{project.title} | <span>{project.category}</span>
+						</h3>
+						<p className={styles.description}>{project.description}</p>
+					</div>
+					<div className={styles.btn_container}>
+						<Link href={project.repository} className={styles.link} target='_blank'>
+							<GlassIcon />
+						</Link>
+						<Link href={project.url} className={styles.link} target='_blank'>
+							<RedirectIcon stroke='#16c0f0' />
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Fade>
 	);
 };
 
