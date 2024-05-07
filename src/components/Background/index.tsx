ad9a347b2c98c3a5633daeafff02b024d9cda9f7
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-07 20:30:20 -0300

    ADD temporary Background

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.scss";
+
+const Background = () => {
+	return (
+		<section className={styles.background}>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</section>
+	);
+};
+
+export default Background;
