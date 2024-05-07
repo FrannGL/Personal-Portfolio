44a4b1332f5a2d5491f4f9682a6d3a0da3fd6125
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-07 18:15:47 -0300

    FIX Card Fade style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,103 +8,96 @@ import DateIcon from "../Icons/Date";
 import { useLayout } from "@/context/LayoutContext";
 
 interface CardProps {
-  id?: number;
-  title: string;
-  description: string;
-  icon?: JSX.Element;
-  image?: StaticImageData;
-  company?: string;
-  dates?: string;
-  badges?: StaticImageData[];
-  certification?: string;
-  category?: string;
-  repository?: string;
-  url?: string;
-  className: string;
-  overlay?: boolean;
+	id?: number;
+	title: string;
+	description: string;
+	icon?: JSX.Element;
+	image?: StaticImageData;
+	company?: string;
+	dates?: string;
+	badges?: StaticImageData[];
+	certification?: string;
+	category?: string;
+	repository?: string;
+	url?: string;
+	className: string;
+	overlay?: boolean;
 }
 
 const Card = ({
-  title,
-  description,
-  icon,
-  image,
-  company,
-  dates,
-  badges,
-  certification,
-  category,
-  repository,
-  url,
-  className,
-  overlay = false,
+	title,
+	description,
+	icon,
+	image,
+	company,
+	dates,
+	badges,
+	certification,
+	category,
+	repository,
+	url,
+	className,
+	overlay = false,
 }: CardProps) => {
-  const { activeRoute } = useLayout();
+	const { activeRoute } = useLayout();
 
-  return (
-    <div className={styles[className]}>
-      {image && <Image src={image} alt="Card Image" className={styles.image} />}
-      {overlay && (
-        <div className={styles.overlay}>
-          <div className={styles.titles}>
-            <h3 className={styles.title}>
-              {title} | <span>{category}</span>
-            </h3>
-            <p className={styles.description}>{description}</p>
-          </div>
-          <div className={styles.btn_container}>
-            <Link
-              href={repository || ""}
-              className={styles.link}
-              target="_blank"
-            >
-              <GlassIcon />
-            </Link>
-            <Link href={url || ""} className={styles.link} target="_blank">
-              <RedirectIcon stroke="#16c0f0" />
-            </Link>
-          </div>
-        </div>
-      )}
-      <div className={styles.content}>
-        {activeRoute === "#contact" ? (
-          <div className={styles.icon_container}>{icon}</div>
-        ) : (
-          <div className={styles.circle}>{icon}</div>
-        )}
-        <div className={styles.bag}></div>
-        <h3 className={styles.title}>
-          {title} {company && <span> | {company}</span>}
-        </h3>
-        {dates && (
-          <div className={styles.dates}>
-            <DateIcon />
-            <p className={styles.date}>{dates}</p>
-          </div>
-        )}
-        <p className={styles.description}>{description}</p>
+	return (
+		<Fade className={styles.fade}>
+			<div className={styles[className]}>
+				{image && <Image src={image} alt='Card Image' className={styles.image} />}
+				{overlay && (
+					<div className={styles.overlay}>
+						<div className={styles.titles}>
+							<h3 className={styles.title}>
+								{title} | <span>{category}</span>
+							</h3>
+							<p className={styles.description}>{description}</p>
+						</div>
+						<div className={styles.btn_container}>
+							<Link href={repository || ""} className={styles.link} target='_blank'>
+								<GlassIcon />
+							</Link>
+							<Link href={url || ""} className={styles.link} target='_blank'>
+								<RedirectIcon stroke='#16c0f0' />
+							</Link>
+						</div>
+					</div>
+				)}
+				<div className={styles.content}>
+					{activeRoute === "#contact" ? (
+						<div className={styles.icon_container}>{icon}</div>
+					) : (
+						<div className={styles.circle}>{icon}</div>
+					)}
+					<div className={styles.bag}></div>
+					<h3 className={styles.title}>
+						{title} {company && <span> | {company}</span>}
+					</h3>
+					{dates && (
+						<div className={styles.dates}>
+							<DateIcon />
+							<p className={styles.date}>{dates}</p>
+						</div>
+					)}
+					<p className={styles.description}>{description}</p>
 
-        {badges && (
-          <div className={styles.badges}>
-            {badges.map((badge, index) => (
-              <Image
-                key={index}
-                src={badge}
-                className={styles.badge}
-                alt={`Badge ${index}`}
-              />
-            ))}
-          </div>
-        )}
+					{badges && (
+						<div className={styles.badges}>
+							{badges.map((badge, index) => (
+								<Image key={index} src={badge} className={styles.badge} alt={`Badge ${index}`} />
+							))}
+						</div>
+					)}
 
-        {certification && (
-          <Link href={certification} target="_blank" className={styles.link}>
-            Ver Certificaciones
-          </Link>
-        )}
-      </div>
-    </div>
-  );
+					{certification && (
+						<Link href={certification} target='_blank' className={styles.link}>
+							Ver Certificaciones
+						</Link>
+					)}
+				</div>
+			</div>
+		</Fade>
+	);
 };
 
 export default Card;
--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -15,183 +15,151 @@ import { POST } from "@/services/fetch";
 import { useTranslations } from "next-intl";
 
 interface Summary {
-  id: number;
-  title: string;
-  description: string;
-  icon: JSX.Element;
+	id: number;
+	title: string;
+	description: string;
+	icon: JSX.Element;
 }
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-  const [checkValidation, setCheckValidation] = useState(false);
-  const errors = useFormValidator(formData);
-  const { notify, notifyError } = useMessageToast();
-  const dict = useTranslations("dict");
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	});
+	const [checkValidation, setCheckValidation] = useState(false);
+	const errors = useFormValidator(formData);
+	const { notify, notifyError } = useMessageToast();
+	const dict = useTranslations("dict");
 
-  const summaryInfo: Summary[] = [
-    {
-      id: 1,
-      title: dict("contact.resume.location"),
-      description: "Buenos Aires, Argentina",
-      icon: <LocationIcon />,
-    },
-    {
-      id: 2,
-      title: dict("contact.resume.email"),
-      description: "francoivangalluccio@gmail.com",
-      icon: <MessageIcon />,
-    },
-    {
-      id: 3,
-      title: dict("contact.resume.phone"),
-      description: "+54 11 2155-7802",
-      icon: <PhoneIcon />,
-    },
-    {
-      id: 4,
-      title: dict("contact.resume.website"),
-      description: "franngl.vercel.app",
-      icon: <WebsiteIcon />,
-    },
-  ];
+	const summaryInfo: Summary[] = [
+		{
+			id: 1,
+			title: dict("contact.resume.location"),
+			description: "Buenos Aires, Argentina",
+			icon: <LocationIcon />,
+		},
+		{
+			id: 2,
+			title: dict("contact.resume.email"),
+			description: "francoivangalluccio@gmail.com",
+			icon: <MessageIcon />,
+		},
+		{
+			id: 3,
+			title: dict("contact.resume.phone"),
+			description: "+54 11 2155-7802",
+			icon: <PhoneIcon />,
+		},
+		{
+			id: 4,
+			title: dict("contact.resume.website"),
+			description: "franngl.vercel.app",
+			icon: <WebsiteIcon />,
+		},
+	];
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const { name, value } = e.target;
+		setFormData(prevFormData => ({
+			...prevFormData,
+			[name]: value,
+		}));
+	};
 
-  const sendForm = async () => {
-    try {
-      const response = await POST("send-email", formData);
-      if (response.statusCode === 201) {
-        notify(dict("toast.success"));
-        setFormData({
-          name: "",
-          email: "",
-          subject: "",
-          message: "",
-        });
-        setCheckValidation(false);
-      } else {
-        notifyError(dict("toast.error_front"));
-      }
-    } catch (e) {
-      notifyError(dict("toast.error_back"));
-      console.log(e);
-    }
-  };
+	const sendForm = async () => {
+		try {
+			const response = await POST("send-email", formData);
+			if (response.statusCode === 201) {
+				notify(dict("toast.success"));
+				setFormData({
+					name: "",
+					email: "",
+					subject: "",
+					message: "",
+				});
+				setCheckValidation(false);
+			} else {
+				notifyError(dict("toast.error_front"));
+			}
+		} catch (e) {
+			notifyError(dict("toast.error_back"));
+			console.log(e);
+		}
+	};
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setCheckValidation(true);
-    if (Object.keys(errors).length === 0) {
-      sendForm();
-    }
-  };
+	const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setCheckValidation(true);
+		if (Object.keys(errors).length === 0) {
+			sendForm();
+		}
+	};
 
-  return (
-    <div className={styles.container}>
-      <Fade>
-        <div className={styles.inner_container}>
-          <Title title={dict("titles.contact")} />
-          <div className={styles.form_container}>
-            <div className={styles.resume_container}>
-              {summaryInfo.map((summary: Summary) => (
-                <Card
-                  key={summary.id}
-                  title={summary.title}
-                  description={summary.description}
-                  icon={summary.icon}
-                  className="card_contact"
-                />
-              ))}
-            </div>
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <Input
-                type="text"
-                name="name"
-                value={formData.name}
-                label={dict("contact.form.name")}
-                placeholder="Carl Johnson ..."
-                onChange={handleChange}
-              />
-              <p
-                className={
-                  checkValidation && errors.name
-                    ? styles.error
-                    : styles.error_hidden
-                }
-              >
-                {errors.name}
-              </p>
-              <Input
-                type="email"
-                name="email"
-                value={formData.email}
-                label={dict("contact.form.email")}
-                placeholder="carljohnson@example.com ..."
-                onChange={handleChange}
-              />
-              <p
-                className={
-                  checkValidation && errors.email
-                    ? styles.error
-                    : styles.error_hidden
-                }
-              >
-                {errors.email}
-              </p>
-              <Input
-                type="text"
-                name="subject"
-                value={formData.subject}
-                label={dict("contact.form.subject")}
-                placeholder={dict("contact.form.subject_placeholder")}
-                onChange={handleChange}
-              />
-              <p
-                className={
-                  checkValidation && errors.subject
-                    ? styles.error
-                    : styles.error_hidden
-                }
-              >
-                {errors.subject}
-              </p>
-              <Input
-                type="textarea"
-                name="message"
-                value={formData.message}
-                label={dict("contact.form.message")}
-                placeholder={dict("contact.form.message_placeholder")}
-                onChange={handleChange}
-              />
-              <p
-                className={
-                  checkValidation && errors.message
-                    ? styles.error
-                    : styles.error_hidden
-                }
-              >
-                {errors.message}
-              </p>
-              <Button title={dict("contact.form.btn")} styleName="second" />
-            </form>
-          </div>
-        </div>
-      </Fade>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.inner_container}>
+				<Title title={dict("titles.contact")} />
+				<div className={styles.form_container}>
+					<Fade className={styles.fade}>
+						<div className={styles.resume_container}>
+							{summaryInfo.map((summary: Summary) => (
+								<Card
+									key={summary.id}
+									title={summary.title}
+									description={summary.description}
+									icon={summary.icon}
+									className='card_contact'
+								/>
+							))}
+						</div>
+					</Fade>
+					<Fade className={styles.fade}>
+						<form className={styles.form} onSubmit={handleSubmit}>
+							<Input
+								type='text'
+								name='name'
+								value={formData.name}
+								label={dict("contact.form.name")}
+								placeholder='Carl Johnson ...'
+								onChange={handleChange}
+							/>
+							<p className={checkValidation && errors.name ? styles.error : styles.error_hidden}>{errors.name}</p>
+							<Input
+								type='email'
+								name='email'
+								value={formData.email}
+								label={dict("contact.form.email")}
+								placeholder='carljohnson@example.com ...'
+								onChange={handleChange}
+							/>
+							<p className={checkValidation && errors.email ? styles.error : styles.error_hidden}>{errors.email}</p>
+							<Input
+								type='text'
+								name='subject'
+								value={formData.subject}
+								label={dict("contact.form.subject")}
+								placeholder={dict("contact.form.subject_placeholder")}
+								onChange={handleChange}
+							/>
+							<p className={checkValidation && errors.subject ? styles.error : styles.error_hidden}>{errors.subject}</p>
+							<Input
+								type='textarea'
+								name='message'
+								value={formData.message}
+								label={dict("contact.form.message")}
+								placeholder={dict("contact.form.message_placeholder")}
+								onChange={handleChange}
+							/>
+							<p className={checkValidation && errors.message ? styles.error : styles.error_hidden}>{errors.message}</p>
+							<Button title={dict("contact.form.btn")} styleName='second' />
+						</form>
+					</Fade>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
