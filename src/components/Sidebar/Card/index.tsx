00414e1f6c5c6dfb73c0ee61d7227b52e91ae25c
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-08 14:31:00 -0300

    FIX Card sidebar logic

--- src/components/Sidebar/Card/index.tsx
+++ src/components/Sidebar/Card/index.tsx
@@ -4,25 +4,27 @@ import { Link } from "@/navigation";
 import { SidebarProps } from "..";
 import { useLayout } from "@/context/LayoutContext";
 
-const Card = ({ title, path, icon, isActive, setActive  }: SidebarProps) => {
-	const { activeRoute, setActiveRoute} = useLayout();
-	const isSelected = activeRoute === path;
+const Card = ({ title, path, icon }: SidebarProps) => {
+  const { activeRoute, setActiveRoute, setIsOpen } = useLayout();
+  const isSelected = activeRoute === path;
 
-	return (
-		<Link
-			className={
-				isSelected
-					? `${styles.container} ${styles.container_selected}`
-					: `${styles.container} ${styles.container_noselected}`
-			}
-			href={path}
-			title={title}
-			onClick={() => setActiveRoute(path)}
-		>
-			{icon}
-			<p className={styles.text}>{title}</p>
-		</Link>
-	);
+  return (
+    <Link
+      className={
+        isSelected
+          ? `${styles.container} ${styles.container_selected}`
+          : `${styles.container} ${styles.container_noselected}`
+      }
+      href={path}
+      title={title}
+      onClick={() => {
+        setActiveRoute(path), setIsOpen(false);
+      }}
+    >
+      {icon}
+      <p className={styles.text}>{title}</p>
+    </Link>
+  );
 };
 
 export default Card;
