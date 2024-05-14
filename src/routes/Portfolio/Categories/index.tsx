2bdf13e0b60d860f6794525034c8ab3f2de91884
Franco Galluccio <francoivangalluccio@gmail.com>
2024-05-14 20:53:20 -0300

    ADD thom's-coffe

--- src/routes/Portfolio/Categories/index.tsx
+++ src/routes/Portfolio/Categories/index.tsx
@@ -2,35 +2,32 @@ import styles from "./styles.module.scss";
 import { useTranslations } from "next-intl";
 
 interface CategoriesProps {
-  setFilter: (filter: string) => void;
+	setFilter: (filter: string) => void;
 }
 
 const Categories = ({ setFilter }: CategoriesProps) => {
-  const dict = useTranslations("dict.portfolio.categories");
+	const dict = useTranslations("dict.portfolio.categories");
 
-  const handleCategoryClick = (category: string) => {
-    setFilter(category);
-  };
+	const handleCategoryClick = (category: string) => {
+		setFilter(category);
+	};
 
-  return (
-    <ul className={styles.list}>
-      <li className={styles.item} onClick={() => handleCategoryClick("All")}>
-        {dict("all")}
-      </li>
-      <li
-        className={styles.item}
-        onClick={() => handleCategoryClick("Frontend")}
-      >
-        {dict("frontend")}
-      </li>
-      <li
-        className={styles.item}
-        onClick={() => handleCategoryClick("Full Stack")}
-      >
-        {dict("full")}
-      </li>
-    </ul>
-  );
+	return (
+		<ul className={styles.list}>
+			<li className={styles.item} onClick={() => handleCategoryClick("All")}>
+				{dict("all")}
+			</li>
+			<li className={styles.item} onClick={() => handleCategoryClick("Frontend")}>
+				{dict("frontend")}
+			</li>
+			<li className={styles.item} onClick={() => handleCategoryClick("Full Stack")}>
+				{dict("full")}
+			</li>
+			<li className={styles.item} onClick={() => handleCategoryClick("Mobile")}>
+				{dict("mobile")}
+			</li>
+		</ul>
+	);
 };
 
 export default Categories;
--- src/routes/Portfolio/index.tsx
+++ src/routes/Portfolio/index.tsx
@@ -9,117 +9,114 @@ import juguetes_perdidos_preview from "/public/assets/juguetes-perdidos.png";
 import maular_preview from "/public/assets/Maular.png";
 import notimation_preview from "/public/assets/Notimation.png";
 import fuego_burgers_preview from "/public/assets/Fuego-burgers.png";
-import imperio_shoes_preview from "/public/assets/imperio-shoes.png";
+import thoms_coffe_preview from "/public/assets/thoms-coffe.png";
 import chapas_qr_preview from "/public/assets/Chapas-qr.png";
 import { Fade } from "react-awesome-reveal";
 import { useTranslations } from "next-intl";
 
 interface Project {
-  id: number;
-  title: string;
-  description: string;
-  category: string;
-  image: StaticImageData;
-  repository: string;
-  url: string;
+	id: number;
+	title: string;
+	description: string;
+	category: string;
+	image: StaticImageData;
+	repository: string;
+	url: string;
 }
 
 const Resume = () => {
-  const [filter, setFilter] = useState<string>("All");
-  const dict = useTranslations("dict");
+	const [filter, setFilter] = useState<string>("All");
+	const dict = useTranslations("dict");
 
-  const projects: Project[] = [
-    {
-      id: 1,
-      title: "Juguetes Perdidos Beer",
-      description: dict("portfolio.projects.description01"),
-      category: "Frontend",
-      image: juguetes_perdidos_preview,
-      repository: "https://github.com/FrannGL/Juguetes-Perdidos-BEER",
-      url: "https://juguetes-perdidos.vercel.app/",
-    },
-    {
-      id: 2,
-      title: "Maular",
-      description: dict("portfolio.projects.description02"),
-      category: "Frontend",
-      image: maular_preview,
-      repository: "https://github.com/FrannGL?tab=repositories",
-      url: "https://www.maular.com/",
-    },
-    {
-      id: 3,
-      title: "Notimation",
-      description: dict("portfolio.projects.description03"),
-      category: "Frontend",
-      image: notimation_preview,
-      repository: "https://github.com/FrannGL?tab=repositories",
-      url: "https://notimation.com/es",
-    },
-    {
-      id: 4,
-      title: "Fuego Burgers",
-      description: dict("portfolio.projects.description04"),
-      category: "Full Stack",
-      image: fuego_burgers_preview,
-      repository: "https://github.com/FrannGL/Fuego-Burgers",
-      url: "https://fuego-burgers.onrender.com/",
-    },
-    {
-      id: 5,
-      title: "Imperio Shoes",
-      description: dict("portfolio.projects.description05"),
-      category: "Full Stack",
-      image: imperio_shoes_preview,
-      repository: "https://github.com/gszcode/E-commerce-Client",
-      url: "https://imperio-shoes.vercel.app/",
-    },
-    {
-      id: 6,
-      title: "Chapas QR",
-      description: dict("portfolio.projects.description06"),
-      category: "Full Stack",
-      image: chapas_qr_preview,
-      repository: "https://github.com/FrannGL/Chapas-qr-frontend",
-      url: "https://chapas-qr.vercel.app/",
-    },
-  ];
+	const projects: Project[] = [
+		{
+			id: 1,
+			title: "Juguetes Perdidos Beer",
+			description: dict("portfolio.projects.description01"),
+			category: "Frontend",
+			image: juguetes_perdidos_preview,
+			repository: "https://github.com/FrannGL/Juguetes-Perdidos-BEER",
+			url: "https://juguetes-perdidos.vercel.app/",
+		},
+		{
+			id: 2,
+			title: "Maular",
+			description: dict("portfolio.projects.description02"),
+			category: "Frontend",
+			image: maular_preview,
+			repository: "https://github.com/FrannGL?tab=repositories",
+			url: "https://www.maular.com/",
+		},
+		{
+			id: 3,
+			title: "Notimation",
+			description: dict("portfolio.projects.description03"),
+			category: "Frontend",
+			image: notimation_preview,
+			repository: "https://github.com/FrannGL?tab=repositories",
+			url: "https://notimation.com/es",
+		},
+		{
+			id: 4,
+			title: "Fuego Burgers",
+			description: dict("portfolio.projects.description04"),
+			category: "Full Stack",
+			image: fuego_burgers_preview,
+			repository: "https://github.com/FrannGL/Fuego-Burgers",
+			url: "https://fuego-burgers.onrender.com/",
+		},
+		{
+			id: 5,
+			title: "Chapas QR",
+			description: dict("portfolio.projects.description05"),
+			category: "Full Stack",
+			image: chapas_qr_preview,
+			repository: "https://github.com/FrannGL/Chapas-qr-frontend",
+			url: "https://chapas-qr.vercel.app/",
+		},
+		{
+			id: 6,
+			title: "Thom's Coffe",
+			description: dict("portfolio.projects.description06"),
+			category: "Mobile",
+			image: thoms_coffe_preview,
+			repository: "https://github.com/FrannGL/thoms-coffe",
+			url: "https://github.com/FrannGL/thoms-coffe",
+		},
+	];
 
-  const filteredProjects =
-    filter === "All"
-      ? projects
-      : projects.filter((project) => project.category === filter);
+	const filteredProjects = filter === "All" ? projects : projects.filter(project => project.category === filter);
 
-  return (
-    <div className={styles.container}>
-      <Fade>
-        <div className={styles.inner_container}>
-          <Title title={dict("titles.portfolio")} />
-          <div className={styles.list_container}>
-            <Categories setFilter={setFilter} />
-          </div>
-          <div className={styles.cards_container}>
-            <Fade cascade damping={0.3} triggerOnce={true}>
-              {filteredProjects.map((project: Project) => (
-                <Card
-                  key={project.id}
-                  id={project.id}
-                  title={project.title}
-                  description={project.description}
-                  category={project.category}
-                  image={project.image}
-                  repository={project.repository}
-                  url={project.url}
-                  className="card_portfolio"
-                  overlay
-                />
-              ))}
-            </Fade>
-          </div>
-        </div>
-      </Fade>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<Fade>
+				<div className={styles.inner_container}>
+					<Title title={dict("titles.portfolio")} />
+					<div className={styles.list_container}>
+						<Categories setFilter={setFilter} />
+					</div>
+					<div className={styles.cards_container}>
+						<Fade cascade damping={0.3} triggerOnce={true}>
+							{filteredProjects.map((project: Project) => (
+								<Card
+									key={project.id}
+									id={project.id}
+									title={project.title}
+									description={project.description}
+									category={project.category}
+									image={project.image}
+									repository={project.repository}
+									url={project.url}
+									className='card_portfolio'
+									overlay
+								/>
+							))}
+						</Fade>
+					</div>
+				</div>
+			</Fade>
+		</div>
+	);
 };
 
 export default Resume;
